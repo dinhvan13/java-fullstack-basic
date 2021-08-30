b8ec7c9f31bc0242415bd6feae44cb4579addd7d
Your Name <63304002+dinhvan13@users.noreply.github.com>
2021-08-30 21:48:25 +0900

    update 2

---
+++ Vue_HVIT/myapp/my-app/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div>
+    <v-icon :size="80"
+    :style="`color: ${turnOn ? '#42b983' : 'grey'}`">mdi-lightbulb</v-icon>
+    <div>
+      <v-btn outlined color ="green" @click="clickOnButton()">
+        Turn {{turnOn ?"Off": "On"}}
+      </v-btn>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  data:() =>({
+    turnOn:false
+  }),
+  methods: {
+     clickOnButton(){
+       this.turnOn = !this.turnOn;
+     }
+  }
+ 
+  };
+
+</script>
